aac111f47947a14619f806f082b9409db909a056
Ariba Khan <92162620+ariba3@users.noreply.github.com>
2023-05-01 21:06:08 +0600

    Add files via upload

---
+++ 10071.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int v, t, s;
+  //  cin>> v>>t;
+
+  //s = v*2*t;
+  //akan a inout koi ses hobe bola hoy ni
+  //end of the file (eof)
+    while(cin>>v>>t){
+        s = v*2*t;
+     cout<<s<<endl;
+    }
+//    cout <<s<<endl;
+
+    return 0;
+}
